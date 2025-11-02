11a234bccd95959e3044524e4d4ed5857b4a26ae
JColeby <jonathan.2022jatc@gmail.com>
2025-11-02 16:36:00 -0700

    polished the code for the editor

--- Commands/BuiltinCommands/Edit/Edit.h
+++ Commands/BuiltinCommands/Edit/Edit.h
@@ -10,19 +10,19 @@
 
 class Edit : public Command<Edit> {
   vector<string> tokenizedCommand;
-  vector<string> status;
-  vector<string> fileContent;
-  int fileX;
-  int fileY;
+  vector<string> status;  // returned when exiting the editor
+  vector<string> fileContent; // stores the current file state
+  int fileX; // x position in the file
+  int fileY; // y position in the file
   bool saved; // keeps track if there have been edits since the file was last saved
-  bool editLoopActive; // flag that will update when we want to kill the program
-  HANDLE inputConsoleHandle;
-  HANDLE outputConsoleHandle;
-  CONSOLE_SCREEN_BUFFER_INFO screenBuffer;
-  SHORT screenWidth;
-  int xOffset;
+  bool editLoopActive; // flag that will update when we want to exit the editor
+  HANDLE inputConsoleHandle; // used for changing console mode and detecting key events
+  HANDLE outputConsoleHandle; // used for displaying output
+  CONSOLE_SCREEN_BUFFER_INFO screenBuffer; // essentially stores the console screen
+  SHORT consoleWidth;  // read the name again silly
+  SHORT consoleHeight;
+  int xOffset; // used to get the cursor and character positions inside screenBuffer
   int yOffset;
-  SHORT screenHeight;
 
 public:
   explicit Edit(vector<string>& tokens) {
@@ -37,6 +37,7 @@ public:
     editLoopActive = true;
     inputConsoleHandle = GetStdHandle(STD_INPUT_HANDLE);
     outputConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    updateScreenBufferInfo();
   }
 
   static string returnManFilePath() {
@@ -48,7 +49,6 @@ public:
     if (tokens.size() == 1 and !tokens[0].empty()) { return true; }
     return false;
   }
-
 
   vector<string> executeCommand() override {
     // Saving the current console mode so we can revert back to it later
@@ -89,8 +89,8 @@ private:
 
   void updateScreenBufferInfo() {
     GetConsoleScreenBufferInfo(outputConsoleHandle, &screenBuffer); // getting our screen buffer
-    screenWidth  = screenBuffer.dwSize.X;
-    screenHeight = screenBuffer.dwSize.Y;
+    consoleWidth  = screenBuffer.dwSize.X;
+    consoleHeight = screenBuffer.dwSize.Y;
   }
 
   // reads in the file and saves it to our buffer
@@ -121,21 +121,17 @@ private:
       status = {"Unknown Internal Error", "500"}; // just in case something goes wrong and status was overwritten
       if (killSwitch) { killSwitchScreen(); }
       // Wait indefinitely for console input
-      DWORD result = WaitForSingleObject(inputConsoleHandle, INFINITE);
-      if (result == WAIT_OBJECT_0) {
-        INPUT_RECORD record;
-        DWORD eventsRead;
+      DWORD result = WaitForSingleObject(inputConsoleHandle, INFINITE); // will wait indefinitely until it detects user input
+      if (result == WAIT_OBJECT_0) { // if user input was detected
 
-        // Read one input event
-        ReadConsoleInput(inputConsoleHandle, &record, 1, &eventsRead);
+        INPUT_RECORD record; // stores info about the input event
+        DWORD eventsRead; // stores the number of events actually read. Only needed for ReadConsoleInput
+        ReadConsoleInput(inputConsoleHandle, &record, 1, &eventsRead); // Reads in the first input event
 
-        if (record.EventType == KEY_EVENT && record.Event.KeyEvent.bKeyDown) {
+        if (record.EventType == KEY_EVENT && record.Event.KeyEvent.bKeyDown) { // if the event was a key down action
           KEY_EVENT_RECORD keyEvent = record.Event.KeyEvent;
-          char ch = keyEvent.uChar.AsciiChar;
-
-          // detecting if ctrl is pressed
-          DWORD mods = keyEvent.dwControlKeyState;
-          bool ctrlDown = (mods & (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED)) != 0;
+          char ch = keyEvent.uChar.AsciiChar; // get the character of the key that was pressed
+          bool ctrlDown = keyEvent.dwControlKeyState & (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED); // detecting if ctrl was also pressed
 
           int newCursorX; // for when we have to update cursorY
           string supportLine; // for dealing with newline changes
@@ -143,12 +139,13 @@ private:
           switch (keyEvent.wVirtualKeyCode) {
             case VK_LEFT:
               if (fileX > 0) { fileX--; }
-              else if (fileY > 0) { fileY--; fileX = fileContent.size() - 1; }
+              else if (fileY > 0) { fileY--; fileX = fileContent[fileY].size() - 1; }
               updateConsoleOffset();
               break;
 
             case VK_RIGHT:
               if (fileX < fileContent[fileY].size()) { fileX++; }
+              else if (fileY < fileContent.size() - 1) { fileY++; fileX = 0; }
               updateConsoleOffset();
               break;
 
@@ -193,27 +190,24 @@ private:
 
             case 'S': // Ctrl+S
               if (ctrlDown and !saved) {
-                try {
-                  saveBufferToFile();
-                  saved = true;
-                }
+                try { saveBufferToFile(); saved = true; }
                 catch (string e) { editError(e); }
               }
-              else { writeCharToBuffer(ch); }
+              else { writeCharToFileInfo(ch); }
               break;
 
             case 'X': // Ctrl+x
               if (ctrlDown) { exitFilePrompt(); }
-              else { writeCharToBuffer(ch); }
+              else { writeCharToFileInfo(ch); }
               break;
 
-            case 'Q': // Ctrl+q (killSwitch)
+            case 'Q': // Ctrl+q (detecting killSwitch because GetAsyncKeyState doesn't work with the current console mode)
               if (ctrlDown) { exitFilePrompt(); }
-              else { writeCharToBuffer(ch); }
+              else { writeCharToFileInfo(ch); }
               break;
 
             default:
-              writeCharToBuffer(ch);
+              writeCharToFileInfo(ch);
           }
         }
       }
@@ -224,16 +218,16 @@ private:
   // updates the screen position relative to the current file
   void updateConsoleOffset() {
     updateScreenBufferInfo();
-    if (fileX < xOffset) { xOffset = fileX; }
-    else if (fileX >= xOffset + screenWidth) { xOffset = fileX - screenWidth + 1; }
-    if (fileY < yOffset) { yOffset = fileY; }
-    else if (fileY >= yOffset + screenHeight) { yOffset = fileY - screenHeight + 1; }
+    while (fileX < xOffset) { xOffset = fileX; } // using while loops for edge cases where it needs to be done multiple times
+    while (fileX >= xOffset + consoleWidth) { xOffset = fileX - consoleWidth + 1; }
+    while (fileY < yOffset) { yOffset = fileY; }
+    while (fileY >= yOffset + consoleHeight) { yOffset = fileY - consoleHeight + 1; }
     if (xOffset < 0) xOffset = 0;
     if (yOffset < 0) yOffset = 0;
   }
 
 
-  void writeCharToBuffer(char ch) {
+  void writeCharToFileInfo(char ch) {
     if (ch >= 32 and ch <= 126) {
       fileContent[fileY].insert(fileX, 1, ch);
       fileX++;
@@ -241,51 +235,53 @@ private:
     }
   }
 
-  // updates the position of the cursor
+
+  // makes sure the cursor is visible and updates its position
   void updateCursor() {
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(outputConsoleHandle, &cursorInfo);
+    cursorInfo.bVisible = TRUE;
+    SetConsoleCursorInfo(outputConsoleHandle, &cursorInfo);
     COORD position = { (SHORT)(fileX-xOffset), (SHORT)(fileY-yOffset) };
     SetConsoleCursorPosition(outputConsoleHandle, position);
   }
 
 
+  // updates file changes to the screen. This code solved the flickering issues with the old updateConsole
   void updateConsole() {
     updateScreenBufferInfo();
 
-    SMALL_RECT writeRegion = { 0, 0, (SHORT)(screenWidth - 1), (SHORT)(screenHeight - 1) };
-    COORD bufferSize = { screenWidth, screenHeight };
+    SMALL_RECT writeRegion = { 0, 0, (SHORT)(consoleWidth - 1), (SHORT)(consoleHeight - 1) };
+    COORD bufferSize = { consoleWidth, consoleHeight };
     COORD zero = { 0, 0 };
+    vector<CHAR_INFO> buffer(consoleWidth * consoleHeight); // Offscreen buffer that will be pushed to the screen
 
-    // Create an offscreen buffer
-    vector<CHAR_INFO> buffer(screenWidth * screenHeight);
-
-    for (SHORT y = 0; y < screenHeight; y++) {
-      for (SHORT x = 0; x < screenWidth; x++) {
-        CHAR_INFO& cell = buffer[y * screenWidth + x]; // get buffer cell
-        if (y < (SHORT)fileContent.size() && x < (SHORT)fileContent[y].size()) {
+    for (SHORT y = 0; y < consoleHeight; y++) {
+      for (SHORT x = 0; x < consoleWidth; x++) {
+        CHAR_INFO& cell = buffer[y * consoleWidth + x]; // get buffer cell
+        if (y < (SHORT)(fileContent.size() - yOffset) && x < (SHORT)(fileContent[y].size() - xOffset)) { // verifying there is a corresponding char for the cell position
           cell.Char.AsciiChar = fileContent[y + yOffset][x + xOffset]; // set buffer cell to the correct ascii character
         } else { cell.Char.AsciiChar = ' '; }
         cell.Attributes = screenBuffer.wAttributes;
       }
     }
-
-    // pushing changes to the screen (won't experience flickering
-    WriteConsoleOutputA(outputConsoleHandle, buffer.data(), bufferSize, zero, &writeRegion);
+    WriteConsoleOutputA(outputConsoleHandle, buffer.data(), bufferSize, zero, &writeRegion); // pushing changes to the screen
     updateCursor();
   }
 
 
-  // deletes character left of this position
-  void deleteCharacter(int charX, int charY, bool updateCursor=true) {
+  // deletes character left of this position. used for both backspace and delete button events.
+  void deleteCharacter(int charX, int charY, bool updatePosition=true) {
     if (charX > 0) { // deleting normal character
       fileContent[charY].erase(charX - 1, 1);
-      if (updateCursor) { fileX--; }
+      if (updatePosition) { fileX--; } // position should only update when backspace is pressed
     }
     else if (charY > 0) { // deleting newline
       int previousLine = fileContent[charY - 1].size();
       fileContent[charY - 1] += fileContent[fileY];
       fileContent.erase(fileContent.begin() + fileY);
       fileY--;
-      if (updateCursor) {
+      if (updatePosition) {
         fileX = previousLine;
       }
     }
@@ -293,26 +289,23 @@ private:
   }
 
 
-  // clears the entire screen and resets cursor position
-  void clearConsole() {
-    system("cls");
-    SetConsoleCursorPosition(outputConsoleHandle, { 0, 0 });
-  }
-
-
-  // prompt that will be shown if killSwitch is true
-  void killSwitchScreen() {
-    exitFilePrompt();
-    status = {"Editor Killed Successfully", "600"};
-  }
-
-
 
   // ===================={ Code for user prompts }====================
 
 
+  // clears the entire screen and hides the cursor.
+  void clearConsole() {
+    system("cls");
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(outputConsoleHandle, &cursorInfo);
+    cursorInfo.bVisible = FALSE; // making the cursor invisible
+    SetConsoleCursorInfo(outputConsoleHandle, &cursorInfo);
+  }
+
+
+
   // prompt that will be shown if a user tries to exit
-  void exitFilePrompt() { // TODO: fix
+  void exitFilePrompt() {
     if (!saved) {
       clearConsole();
       cout << "You have unsaved changes. Do you want to save before exiting?" << "\n";
@@ -334,30 +327,6 @@ private:
   }
 
 
-  // will return one of the keys in the provided vector when it is pressed. Will wait until one of the keys is pressed
-  int returnKeyPress(vector<int> keys, bool anyKey=false) {
-    FlushConsoleInputBuffer(inputConsoleHandle); // flushes the buffer so we don't pick up keys that were pressed before calling
-    while (true) {
-      DWORD result = WaitForSingleObject(inputConsoleHandle, INFINITE);
-      if (result == WAIT_OBJECT_0) {
-        INPUT_RECORD record;
-        DWORD eventsRead;
-        ReadConsoleInput(inputConsoleHandle, &record, 1, &eventsRead); // read in input
-
-        if (record.EventType == KEY_EVENT and record.Event.KeyEvent.bKeyDown) {
-          if (anyKey) { return record.Event.KeyEvent.wVirtualKeyCode; } // returns the key that was pressed if anyKey is True
-
-          KEY_EVENT_RECORD keyEvent = record.Event.KeyEvent;
-          char ch = keyEvent.uChar.AsciiChar;
-          for (int i = 0; i < keys.size(); i++) {
-            if (tolower(ch) == tolower(keys[i])) return ch; // use tolower so the keys are still detected if uppercase
-          }
-
-        }
-      }
-    }
-  }
-
   // used for prompts. will freeze the screen until a key is pressed
   void hitAnyKeyToContinue() {
     cout << "hit any key to continue" << "\n";
@@ -371,6 +340,37 @@ private:
     clearConsole();
     cout << "EDIT ERROR:" << e << "\n";
     hitAnyKeyToContinue();
+  }
+
+
+  // prompt that will be shown if killSwitch is true
+  void killSwitchScreen() {
+    exitFilePrompt();
+    status = {"Editor Killed Successfully", "600"};
+  }
+
+
+  // will return one of the keys in the provided vector when it is pressed. Will wait until one of the keys is pressed
+  int returnKeyPress(vector<int> keys, bool anyKey=false) {
+    FlushConsoleInputBuffer(inputConsoleHandle); // flushes the buffer so we don't pick up keys that were pressed before calling
+
+    while (true) { // code is the same as what's in editFile. Look there for more information
+      DWORD result = WaitForSingleObject(inputConsoleHandle, INFINITE);
+      if (result == WAIT_OBJECT_0) {
+        INPUT_RECORD record;
+        DWORD eventsRead;
+        ReadConsoleInput(inputConsoleHandle, &record, 1, &eventsRead);
+        if (record.EventType == KEY_EVENT and record.Event.KeyEvent.bKeyDown) {
+          KEY_EVENT_RECORD keyEvent = record.Event.KeyEvent;
+          char ch = keyEvent.uChar.AsciiChar;
+
+          if (anyKey) { return record.Event.KeyEvent.wVirtualKeyCode; } // returns the key that was pressed if anyKey is True
+          for (int i = 0; i < keys.size(); i++) {
+            if (tolower(ch) == tolower((char)keys[i])) return ch; // use tolower so the keys are still detected if uppercase
+          }
+        }
+      }
+    }
   }
 
 
