ff4a29176d53e13cdc1db1180237bf664105df24
JColeby <jonathan.2022jatc@gmail.com>
2025-11-21 18:25:37 -0700

    Added Date command and updated SystemInfo

--- Commands/BuiltinCommands/SystemInfo/SystemInfo.h
+++ Commands/BuiltinCommands/SystemInfo/SystemInfo.h
@@ -58,7 +58,7 @@ public:
       outputBuffer << getMemoryInfo();
     }
     if (getDrive) {
-      outputBuffer << getDiskInfo();
+      outputBuffer << getDriveInfo();
     }
     if (getPerformance) {
       outputBuffer << getPerformanceInfo();
@@ -201,7 +201,7 @@ private:
   }
 
 
-  static string getDiskInfo() {
+  static string getDriveInfo() {
     // Querying the drives available.
     // GetLogicalDrives returns a bitmask that represents disk drives that are currently available
     // Bit position 0 (the least-significant bit) is drive A, bit position 1 is drive B, bit position 2 is drive C, and so on.
@@ -214,7 +214,16 @@ private:
     }
 
     std::stringstream outputBuffer;
-    outputBuffer << WHITE << "\n====={ Drive/Disk Information }=====\n" << RESET_TEXT;
+    outputBuffer << WHITE << "\n====={ Drive Information }=====\n" << RESET_TEXT;
+
+
+    // getting physical drive information
+    for (int i = 0; i < 32; i++) {
+      string physicalDriveOutput = getPhysicalDriveInfo(i);
+      if (physicalDriveOutput == "") { break; }
+      outputBuffer << physicalDriveOutput;
+    }
+
 
     for (wchar_t i = 'A'; i <= 'Z'; i++) {
       if (driveBitmask & 1) {
@@ -239,6 +248,98 @@ private:
 
     return outputBuffer.str();
   }
+
+
+
+
+  // returns physical drive info. If the physical drive doesn't exist, the function returns an empty string
+  // because there isn't a set function that will retrieve this info for us, we have use device/driver queries
+  static string getPhysicalDriveInfo(int driveIndex) {
+    std::stringstream outputBuffer;
+
+    // Building the path to the physical drive
+    std::wstring path = L"\\\\.\\PhysicalDrive" + std::to_wstring(driveIndex);
+
+    // This function creates or opens a file or I/O device.
+    // for more information about what this is doing, please visit https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
+    HANDLE diskHandle = CreateFileW(path.c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE,
+        nullptr, OPEN_EXISTING, 0, nullptr );
+
+    // if the drive we are looking for does not exist
+    if (diskHandle == INVALID_HANDLE_VALUE) { return ""; }
+
+    // Because the physical drive is an I/O device, we have to call DeviceIoControl to get the information
+    // Unfortunately this function is the biggest multi-tool of a function that I have ever seen, so we have to
+    // specify what type of query we are making using this structure
+    STORAGE_PROPERTY_QUERY driveQuery = {};
+    driveQuery.PropertyId = StorageDeviceProperty;
+    driveQuery.QueryType = PropertyStandardQuery;
+
+    // Because of the nature of this function, what gets returned will vary depending on what query we make.
+    // This means that we have to create a generic buffer to store the info from the query.
+    // In order to get the correct size for our buffer, we first have to get the header information.
+    // We do this by querying DeviceIoControl twice, where the first query will retrieve the response header, which we know the size of.
+    STORAGE_DESCRIPTOR_HEADER driveQueryHeader = {};
+    DWORD bytesReturned = 0;
+
+    // DeviceIoControl is smart and will prevent itself from going beyond the buffer. Because of this, we can just
+    // pass in the header structure, which will contains all the additional information that will be needed for the actual query
+    if (!DeviceIoControl(
+            diskHandle, // this is the handle to our drive that we got earlier
+            IOCTL_STORAGE_QUERY_PROPERTY, // this tells the function what type of query we are performing (known as the control code)
+            &driveQuery, // this is the input buffer, which we pass in our query. The query type is associated with the control code
+            sizeof(driveQuery), // size of our query
+            &driveQueryHeader, // this is the buffer where our output will be stored
+            sizeof(driveQueryHeader), // size of the output buffer
+            &bytesReturned, // the number of bytes that were filled in our output buffer
+            nullptr)) // last parameter is for asynchronous calls. I didn't dive in too deep because it's not needed
+    {
+      std::stringstream errorMsg;
+      errorMsg << RED << "\nERROR: Failed to get physical drive info for \\\\.\\PhysicalDrive" + std::to_string(driveIndex) << "\n" << RESET_TEXT;
+      return errorMsg.str();
+    }
+
+    DWORD requiredSize = driveQueryHeader.Size; // now we have the size of the full query, which we can use to make a proper buffer
+    vector<BYTE> buffer(requiredSize);
+
+    // now we can call it with our proper buffer that will contain everything from the call.
+    if (!DeviceIoControl(diskHandle, IOCTL_STORAGE_QUERY_PROPERTY, &driveQuery, sizeof(driveQuery),
+      buffer.data(), requiredSize, &bytesReturned, nullptr))
+    {
+      std::stringstream errorMsg;
+      errorMsg << RED << "\nERROR: Failed to get physical drive info for \\\\.\\PhysicalDrive" + std::to_string(driveIndex) << "\n" << RESET_TEXT;
+      return errorMsg.str();
+    }
+
+    // now we just have to redefine the buffer so the compiler knows it contains a STORAGE_DEVICE_DESCRIPTOR
+    STORAGE_DEVICE_DESCRIPTOR* driveDescriptor = reinterpret_cast<STORAGE_DEVICE_DESCRIPTOR*>(buffer.data());
+
+
+
+    outputBuffer << "  PhysicalDrive" << driveIndex << ": " << "\n";
+
+    // now windows was silly, and decided that the function wouldn't actually include information like the
+    // VendorId or ProductId in the structure. Instead, they included offsets to the actual information, which is stored
+    // after the STORAGE_DEVICE_DESCRIPTOR object in the buffer (stupid, I know).
+    string model;
+    if (driveDescriptor->ProductIdOffset != 0) { // extracting the Product ID, which contains the model of our drive
+      model = reinterpret_cast<const char*>(sizeof(buffer) + driveDescriptor->ProductIdOffset);
+    } else { model = "UnknownModel"; }
+    outputBuffer << "    Drive Model: " << model << "\n";
+
+    string vendor;
+    if (driveDescriptor->VendorIdOffset != 0) { // extracting the Product ID, which contains the model of our drive
+      vendor = reinterpret_cast<const char*>(sizeof(buffer) + driveDescriptor->VendorIdOffset);
+    } else { vendor = "UnknownVendor"; }
+    outputBuffer << "    Drive Vendor: " << vendor << "\n";
+
+    if (driveDescriptor->RemovableMedia) { outputBuffer << "    Removable Media\n"; }
+    else { outputBuffer << "    Installed Media\n"; }
+
+  }
+
+
+
 };
 
 
