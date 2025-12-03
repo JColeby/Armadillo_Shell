aa40822d81facee6be40adb3dda818a00c9d0aef
Chase Cook <chasefcook@gmail.com>
2025-12-03 16:49:42 -0700

    manuals

--- Commands/BuiltinCommands/Cat/Manual.h
+++ Commands/BuiltinCommands/Cat/Manual.h
@@ -2,12 +2,6 @@
 #include <string>
 
 const std::string CatManual = R"(
-This file will contain the text that shows up when you call the manual command for this class.
-All the code/files for the command should remain inside its own folder.
-The name of this file should always be Manual.txt
-When writing the manual, make sure to include valid flags
-
-for whoever is creating the manual command, import "importAllCommands.h" so you don't have to define each command,
-this will be useful for getting the manual file path
-
+Syntax:
+    cat <filename>
 )";
--- Commands/BuiltinCommands/List/Manual.h
+++ Commands/BuiltinCommands/List/Manual.h
@@ -2,12 +2,9 @@
 #include <string>
 
 const std::string ListManual = R"(
-This file will contain the text that shows up when you call the manual command for this class.
-All the code/files for the command should remain inside its own folder.
-The name of this file should always be Manual.txt
-When writing the manual, make sure to include valid flags
+Syntax:
+    ls <flags>
 
-for whoever is creating the manual command, import "importAllCommands.h" so you don't have to define each command,
-this will be useful for getting the manual file path
-
+Flags:
+    -a  displays hidden contents of current directory
 )";
